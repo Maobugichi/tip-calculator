fd57c98514bcaf2b1eba1157a5053b9015f4731a
Maobugichi <immansamuel846@gmail.com>
2024-10-12 12:50:05 -0700

    adjust validation

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ item.addEventListener("click", getStoredItems) })
 function getStoredItems(e) {
    if (!storedItems.text || !storedItems.people || !Object.keys(storedItems).length ) {
       input1.forEach(item => {
-      if (!storedItems[item.name]) {
+      if (!storedItems[item.name] && item.name !== "custom") {
          item.classList.add("active");
       } else if (storedItems[item.name]) {
          if (prev) {
@@ -102,94 +102,15 @@ function getStoredItems(e) {
            const total =    Math.round((tipPrice + amountPaid) * 100) / 100;
            ta.innerText = tipPrice ? `$${tipPrice}` : "$00.00"
            ta2.innerText = total ? `$${total}` : "$00.00"      
-        }
-          
-         })
-      
+        }    
+      })  
    }
-   
-   
-   
 }
 
 
-
-/*input1.forEach(item => {
- item.addEventListener("keyup",checkValue)
- //console.log(item)
-})
-
-function checkValue(e) {
- storedItems = {...storedItems,[e.target.name]: e.target.value}
-}
-
-function checkEmpty() {
- input1.forEach((input,index) => {
- //console.log(input.name)
- if (input.name !== "custom") {
- const isEmpty = !input.value || parseInt(input.value) <= 0;
- input.classList.toggle("active", isEmpty);
- }
- 
- //console.log(isEmpty)
-});
-}
-
-function checkStored() {
- console.log(storedItems)
- if (!storedItems.length) {
-   console.log("spa")
-    spa[1].classList.remove("show")
-    spa[0].classList.remove("show")
-    //console.log(storedItems)
-    return true
-} else {
-    input1.forEach((item,index) => {
-      console.log(item.name)
-     if (item.value == 0) {
-      console.log(spa)
-        spa[1].classList.add("show")
-        spa[0].classList.add("show")
-     } else if (item.value !== 0) {
-        spa[1].classList.remove("show")
-        spa[0].classList.remove("show")
-     }
-  })    
- }
-}
-
-custom.addEventListener("keyup", (e) => {
- if (checkStored()) {
-    checkEmpty()
-  
- }  
-          
-})
-
-btn.forEach((item,e) => {
- item.addEventListener("click", (e) => {
-  checkEmpty()
-  console.log(storedItems)
-  if (checkStored()) {
-  let bill = parseFloat(storedItems.text) 
-  let peopleAmt = parseInt(storedItems.people) 
-  let tipPercent =  parseInt(e.target.value)
-  const billTotal = bill  / peopleAmt
-  const tipPrice = tipPercent / 100 
-  const psk = tipPrice * billTotal
- //const total = billTotal + psk
-  const result = Math.round(psk * 100 - 0.5) / 100 ;
-  const total =  Math.round((billTotal + psk) * 100) / 100;
-  ta.innerText = result ? `$${result}` : "$00.00"
-  ta2.innerText = total ? `$${total}` : "$00.00"
-
-  }  
- })
-})        
-//console.log(storedItems.custom)
 function resett() {
-    ta.innerText = "$00.00"
-    ta2.innerText = "$00.00"
+   ta.innerText = "$00.00"
+   ta2.innerText = "$00.00"
 }
-reset.addEventListener("click",resett)*/
+reset.addEventListener("click",resett)
 
